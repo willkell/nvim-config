2dc4d026b4d934509922a2f800cace228eeebc4e
willkell <willkelley1202@gmail.com>
2024-04-28 22:00:05 -0400

    updated gitignore and formatting

--- init.lua
+++ init.lua
@@ -59,7 +59,7 @@ require('lazy').setup({
     {
         "nvim-neorg/neorg",
         dependencies = { "luarocks.nvim" },
-        lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+        lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
         version = "*", -- Pin Neorg to the latest stable release
         config = true,
     },
