4614267c9cba987e6a2c5aad56bb09e49d9c4de3
Will Kelley <wkelley@nasba.org>
2025-06-09 12:01:23 -0500

    format options and make the powershell stuff windows only

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,29 +14,30 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.swapfile = false
 vim.o.backup = false
-vim.o.undodir = vim.fn.stdpath("config") .. '/undodir'
+vim.o.undodir = vim.fn.stdpath("config") .. "/undodir"
 vim.o.undofile = true
 vim.o.incsearch = true
 vim.o.scrolloff = 6
-vim.o.colorcolumn = '100'
-vim.o.signcolumn = 'yes'
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.o.colorcolumn = "100"
+vim.o.signcolumn = "yes"
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.o.termguicolors = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.laststatus = 3
 vim.o.mousemodel = extend
 
-local powershell_options = {
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag =
-    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-}
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+	local powershell_options = {
+		shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+		shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+		shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+		shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+		shellquote = "",
+		shellxquote = "",
+	}
 
-for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
+	for option, value in pairs(powershell_options) do
+		vim.opt[option] = value
+	end
 end
