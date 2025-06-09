74a4de616dda2388058ef3c2f8642aca9c79525a
Will Kelley <wkelley@nasba.org>
2025-06-09 10:16:45 -0500

    correctly load dotenv

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -8,6 +8,13 @@ return {
 			vim.g.startuptime_tries = 10
 		end,
 	},
-	"ellisonleao/dotenv.nvim",
+	{
+		"ellisonleao/dotenv.nvim",
+		config = function()
+			require("dotenv").setup({
+                enable_on_load = true,
+            })
+		end,
+	},
 	{ "vhyrro/luarocks.nvim", priority = 1000, config = true },
 }
