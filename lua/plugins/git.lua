b7567e518317c021c7d4842f346b07eab84e70e5
Will Kelley <wkelley@nasba.org>
2025-06-12 15:27:14 -0500

    change lazygit plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,25 +2,4 @@ return {
 	{ "lewis6991/gitsigns.nvim", event = "VeryLazy" },
 	{ "f-person/git-blame.nvim", lazy = true },
 	{ "tpope/vim-fugitive", event = "CmdLineEnter" },
-	{
-		"kdheepak/lazygit.nvim",
-		lazy = true,
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-		config = function()
-			vim.g.lazygit_floating_window_scaling_factor = 0.8 -- scaling factor for floating window
-		end,
-	},
 }
