c3895333d29164842197f153f38ea2be785cedc0
Will Kelley <wkelley@nasba.org>
2025-06-27 13:51:42 -0500

    switched out some plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,8 +2,14 @@ local enterFileEvent = { "BufReadPost", "BufNewFile", "BufWritePost" }
 
 return {
 	{ "numToStr/comment.nvim", opts = {}, event = "VeryLazy" },
-	{ "tpope/vim-repeat", event = "VeryLazy" },
-	{ "tpope/vim-surround", event = "VeryLazy" },
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
@@ -124,7 +130,7 @@ return {
 					python = { "black" },
 					javascript = { "prettier" },
 					typescriptreact = { "prettier" },
-					html = { "prettier" },
+					html = { "prettier"},
 				},
 			})
 		end,
@@ -216,11 +222,19 @@ return {
 		end,
 	},
 	{
-		"ggandor/leap.nvim",
-		config = function()
-			require("leap").set_default_mappings()
-		end,
-	},
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+},
 	{ "nvim-treesitter/nvim-treesitter-context", opts = {}, event = enterFileEvent },
 	{
 		"nmac427/guess-indent.nvim",
