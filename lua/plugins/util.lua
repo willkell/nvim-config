ad67096086563a4f6e2d816ce50e2c6813184686
Will Kelley <willkelley1202@gmail.com>
2025-07-10 14:38:01 -0500

    changed dashboard a bit

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -24,15 +24,15 @@ return {
 			dashboard = {
 				sections = {
 					{ section = "header" },
-					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{ section = "keys", gap = 1, padding = 2},
 					{
 						icon = " ",
 						title = "Recent Files",
 						section = "recent_files",
 						indent = 2,
-						padding = 1,
+						padding = 2,
 					},
-					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
 					{ section = "startup" },
 				},
 			},
