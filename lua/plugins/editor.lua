7860141f4553b50b0ab970a7af7d03386ef8b937
Will Kelley <willkelley1202@gmail.com>
2026-06-13 15:45:02 -0500

    fix nvim ufo

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -131,11 +131,17 @@ return {
 		dependencies = "kevinhwang91/promise-async",
 		event = enterFileEvent,
 		config = function()
-			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldcolumn = "1"
 			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
-			require("ufo").setup()
+			-- use Neovim nightly branch
+			vim.o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:"
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "treesitter", "indent" }
+				end,
+			})
 
 			-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
